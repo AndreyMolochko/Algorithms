b83090be12a72030fcd067ffd411e4afa003d414
AndreyMolochko <Molochko_AG_16@student.grsu.by>
2018-03-25 17:11:12 +0300

    merge sort

--- laba4/laba4/Sortings.cs
+++ laba4/laba4/Sortings.cs
@@ -20,6 +20,10 @@ namespace laba4
             FillDataArray(a);
             FillDataArray(b);
             FillDataArray(c);
+            Output(a);
+            a=MergeSort(a);
+            Console.WriteLine("___________");
+            Output(a);
             
         }
         public void FillDataArray(int []a)
@@ -79,6 +83,32 @@ namespace laba4
                     arr[min] = temp;
                 }
             }
+        }
+        public static int[] MergeSort(int[] massive)
+        {
+            if (massive.Length == 1)
+                return massive;
+            int mid_point = massive.Length / 2;
+            return Merge(MergeSort(massive.Take(mid_point).ToArray()), MergeSort(massive.Skip(mid_point).ToArray()));
+        }
+        public static int[] Merge(int[] mass1, int[] mass2)
+        {
+            int a = 0, b = 0;
+            int[] merged = new int[mass1.Length + mass2.Length];
+            for (int i = 0; i < mass1.Length + mass2.Length; i++)
+            {
+                if (b < mass2.Length && a < mass1.Length)
+                    if (mass1[a] > mass2[b] && b < mass2.Length)
+                        merged[i] = mass2[b++];
+                    else
+                        merged[i] = mass1[a++];
+                else
+                    if (b < mass2.Length)
+                    merged[i] = mass2[b++];
+                else
+                    merged[i] = mass1[a++];
+            }
+            return merged;
         }
     }
 }
